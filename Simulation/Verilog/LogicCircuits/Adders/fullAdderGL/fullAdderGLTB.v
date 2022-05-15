40fe265bff5713758bcee334fa0defa89fc5acc3
darleg <darleg@outlook.com>
2022-05-14 18:52:18 -0700

    Update fullAdderGLTB.v

--- Simulation/Verilog/LogicCircuits/Adders/fullAdderGL/fullAdderGLTB.v
+++ Simulation/Verilog/LogicCircuits/Adders/fullAdderGL/fullAdderGLTB.v
@@ -10,12 +10,12 @@ wire Carry;
 fullAdderGL fAGL (.a(A),.b(B),.cin(Cin),.sum(Sum),.carry(Carry));
 
 initial begin
- A = 0; B = 0;
+$dumpfile("wave.vcd");
+$dumpvars();
+$monitor ("T=%0t,A=%b,B=%b,Cin=%b,Sum=%b,Carry=%b",$time,A,B,Cin,Sum,Carry);
+A = 0; B = 0;
 #10; A = 1; B = 0; Cin = 0;
 #10; A = 0; B = 1; Cin = 0;
 #10; A = 1; B = 1; Cin = 1;
-end
-initial begin
-$monitor ("T=%0t,A=%b,B=%b,Cin=%b,Sum=%b,Carry=%b",$time,A,B,Cin,Sum,Carry);
 end
 endmodule
