84f9c8acad1f8861bdd7437ed090a4c4b596a102
darleg <darleg@outlook.com>
2022-05-15 11:58:22 -0700

    update salu

--- Simulation/Verilog/LogicCircuits/ALU/salu/salu.v
+++ Simulation/Verilog/LogicCircuits/ALU/salu/salu.v
@@ -4,7 +4,7 @@ module salu(operanda,operandb,mux,result);
     
     //inputs,outputs and wires
     input [7:0] operanda,operandb;
-    input [2:0] mux;
+    input [3:0] mux;
     output [7:0] result;
     wire [7:0] op1,op2;
     reg [7:0] r;
@@ -18,14 +18,18 @@ module salu(operanda,operandb,mux,result);
     always @(mux or op1 or op2)
     begin
         case (mux)
-         0 : r = op1 + op2;    //Add
-         1 : r = op1 - op2;    //Sub
-         2 : r = ~op1;         //NOT
-         3 : r = op1 & op2;    //AND
-         4 : r = op1 | op2;    //OR
-         5 : r = ~(op1 & op2); //NAND 
-         6 : r = ~(op1 | op2); //NOR                
-         7 : r = op1 ^ op2;    //XOR
+         'h0 : r = op1 + op2;    //Add
+         'h1 : r = op1 - op2;    //Sub
+         'h2 : r = op1 * op2;    //Mul
+         'h3 : r = op1 / op2;    //Div
+         'h4 : r = ~op1;         //NOT
+         'h5 : r = op1 & op2;    //AND
+         'h6 : r = op1 | op2;    //OR
+         'h7 : r = ~(op1 & op2); //NAND 
+         'h8 : r = ~(op1 | op2); //NOR                
+         'h9 : r = op1 ^ op2;    //XOR
+         'hA : r = op1 << 1;     //Logical shift left
+         'hB : r = op1 >> 1;     //Logical shift right
         endcase 
     end
     
--- Simulation/Verilog/LogicCircuits/ALU/salu/salu_tb.v
+++ Simulation/Verilog/LogicCircuits/ALU/salu/salu_tb.v
@@ -3,7 +3,7 @@ module salu_tb;
     // Inputs
     reg [7:0] opA;
     reg [7:0] opB;
-    reg [2:0] Mux;
+    reg [3:0] Mux;
 
     // Outputs
     wire [7:0] Result;
@@ -14,20 +14,23 @@ module salu_tb;
     initial begin
       $dumpfile("wave.vcd");
       $dumpvars(); 
-      $monitor("simtime=%g,opA=%b,opB=%b,Mux=%b,Result=%b",$time,opA,opB,Mux,Result);
+      $monitor("simtime=%g,opA=%b,opB=%b,Mux=%h,Result=%b",$time,opA,opB,Mux,Result);
       // Apply inputs.
         opA = 8'b01101010;
         opB = 8'b00111011;
         // Multiplex
-        Mux = 0; #10;
-        Mux = 1; #10;
-        Mux = 2; #10;
-        Mux = 3; #10;
-        Mux = 4; #10;
-        Mux = 5; #10;
-        Mux = 6; #10;
-        Mux = 7; #10;
-        //#10 $finish;
+        Mux = 'h0; #10;
+        Mux = 'h1; #10;
+        Mux = 'h2; #10;
+        Mux = 'h3; #10;
+        Mux = 'h4; #10;
+        Mux = 'h5; #10;
+        Mux = 'h6; #10;
+        Mux = 'h7; #10;
+        Mux = 'h8; #10;
+        Mux = 'h9; #10;
+        Mux = 'hA; #10;
+        Mux = 'hB; #10;
     end
       
 endmodule
